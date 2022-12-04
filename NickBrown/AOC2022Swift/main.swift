701a579c96b98a186b57671f8c726d23d67b1b3b
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2022-12-04 17:48:23 +0000

    added timing

--- NickBrown/AOC2022Swift/main.swift
+++ NickBrown/AOC2022Swift/main.swift
@@ -7,6 +7,9 @@
 // REPL available at https://replit.com/@nickbrown1987/AdventOfCode22
 
 import Foundation
+import QuartzCore
+
+let start = CACurrentMediaTime()
 
 func day_1(){
     let a = day1(input_file:"/Users/Nick/Documents/GitHub/AdventOfCode/NickBrown/AOC2022Swift/day1.txt")
@@ -14,6 +17,8 @@ func day_1(){
     print("Day 1 Part 2: \(a.topThree())")
 }
 day_1()
+let day1finish = CACurrentMediaTime()
+print("Day 1 execution time: \(day1finish - start)")
 
 func day_2(){
     let b = Day2(input_file:"/Users/Nick/Documents/GitHub/AdventOfCode/NickBrown/AOC2022Swift/day2.txt")
@@ -22,6 +27,8 @@ func day_2(){
     print("Day 2 Part 2: \(b.pt2())")
 }
 day_2()
+let day2finish = CACurrentMediaTime()
+print("Day 2 execution time: \(day2finish - day1finish)")
 
 func day_3(){
     let c = Day3(filename: "/Users/Nick/Documents/GitHub/AdventOfCode/NickBrown/AOC2022Swift/day3.txt")
@@ -30,6 +37,8 @@ func day_3(){
     print("Day 3 Part 2: \(c.secondSum)")
 }
 day_3()
+let day3finish = CACurrentMediaTime()
+print("Day 3 execution time: \(day3finish - day2finish)")
 
 func day_4(){
     let d = Day4(input_file_: "/Users/Nick/Documents/GitHub/AdventOfCode/NickBrown/AOC2022Swift/day4.txt")
@@ -37,3 +46,5 @@ func day_4(){
     print("Day 4 Part 2: \(d.pt2)")
 }
  day_4()
+let day4finish = CACurrentMediaTime()
+print("Day 4 execution time: \(day4finish - day3finish)")
