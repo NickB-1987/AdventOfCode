8bb1b3def49340aa445734ec7a3b40f3a5d262d8
NickB-1987 <94359987+NickB-1987@users.noreply.github.com>
2022-12-14 23:52:00 +0000

    day 14 complete, 13 shelved for now

---
+++ NickBrown/AOC2022Swift/day13.swift
@@ -0,0 +1,8 @@
+//
+//  day13.swift
+//  AOC2022Swift
+//
+//  Created by Nick Brown on 13/12/2022.
+//
+
+import Foundation
---
+++ NickBrown/AOC2022Swift/day14.swift
@@ -0,0 +1,8 @@
+//
+//  day14.swift
+//  AOC2022Swift
+//
+//  Created by Nick Brown on 14/12/2022.
+//
+
+import Foundation
